768320965c5c0a564a8018f68b17ed883f6476c5
Dwayne Weller <mr_dwayne_w@yahoo.com>
2021-01-06 07:06:21 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {
+        ...state,
+        friend: action.friend
+      }
+      break;
+    default:
+      
+  }
 }
